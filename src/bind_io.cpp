b5e4f79a4734b69260c3517547caacf16c5582f1
Jan Górski <jan.a.gorski@wp.pl>
2024-11-27 23:44:31 +0100

    added requirement to import numpy

--- src/bind_io.cpp
+++ src/bind_io.cpp
@@ -4,17 +4,28 @@
 
 namespace py = pybind11;
 
+void ensure_numpy() {
+    try {
+        py::module sys = py::module_::import("sys");
+        py::object modules = sys.attr("modules");
+
+        if (!modules.contains("numpy")) {
+        throw std::runtime_error("'numpy' is required but not imported");
+        }
+    } catch (const py::error_already_set&) {
+        throw std::runtime_error("an error occured while checking if 'numpy' is imported");
+    }
+}
+
 PYBIND11_MODULE(musio, m) {
+    ensure_numpy();
     m.doc() = "Python bindings for the musio library";
 
-    // Bind the load function, converting std::vector<double> to numpy array
     m.def("load", [](const char* filename) -> py::array_t<double> {
         std::vector<double> data = musio::load(filename);
 
-        // Create a numpy array from the std::vector
         py::array_t<double> numpy_array(data.size(), data.data());
 
-        // Return the numpy array
         return numpy_array;
     }, "Load data from a file and return as a numpy array of float64");
 }
