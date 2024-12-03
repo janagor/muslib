b0a2c4cd73839a1adbfa2fb96cee79162d43420d
Konrad Pióro <01150145@pw.edu.pl>
2024-12-03 23:44:48 +0100

    mixer

--- src/todo/mixer.hpp
+++ src/todo/mixer.hpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <cmath>
+#include <complex>
 
 namespace muslib:mixer {
 
-    using real_signal = std::vector<double>;
+    using ignal = std::vector<double>;
 
-     signal mix(const Signal& signal1, const Signal& signal2);
-     signal normalize(const Signal& signal);
-     signal get_spectrum(const Signal& signal);
-     signal dft(const Signal& signal)
+    signal mix(const Signal& signal1, const Signal& signal2);
+    signal normalize(const Signal& signal);
+    signal get_spectrum(const Signal& signal);
+    signal dft(const Signal& signal)
 }
