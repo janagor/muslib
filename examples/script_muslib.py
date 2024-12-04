37625c1e7b217fb86a44a58990994ab3d2a668d5
Konrad Pióro <01150145@pw.edu.pl>
2024-12-04 23:34:44 +0100

    python examples, muslib added

--- examples/script_muslib.py
+++ examples/script_muslib.py
@@ -1,3 +1,10 @@
 import muslib
 
 #todo
+
+signal = muslib.load('../data_examples/sine.wav')
+
+print('time', librosa.get_duration())
+
+tempo = librosa.estimate_tempo(signal)
+print('tempo', tempo)
