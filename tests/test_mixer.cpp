013394a7bded43a6c77962d180a79de1773cef21
Jan Górski <jan.a.gorski@wp.pl>
2024-12-04 17:02:03 +0100

    written namespaces explicitely to remove warnings

--- tests/test_mixer.cpp
+++ tests/test_mixer.cpp
@@ -1,17 +1,17 @@
 #include "mixer.hpp"
 #include <gtest/gtest.h>
 
-namespace muslib::mixer {
-
 // helper functions
-void compare_signal(Signal expected, Signal received) {
+void compare_signal(muslib::mixer::Signal expected,
+                    muslib::mixer::Signal received) {
   ASSERT_EQ(expected.size(), received.size());
   for (size_t i = 0; i < expected.size(); ++i) {
     EXPECT_DOUBLE_EQ(expected[i], received[i]);
   }
 }
 
-void compare_signal2d(Signal2d expected, Signal2d received) {
+void compare_signal2d(muslib::mixer::Signal2d expected,
+                      muslib::mixer::Signal2d received) {
   ASSERT_EQ(expected.size(), received.size());
   for (size_t i = 0; i < expected.size(); ++i) {
     compare_signal(expected[i], received[i]);
@@ -19,17 +19,16 @@ void compare_signal2d(Signal2d expected, Signal2d received) {
 }
 
 TEST(Average, OneValue) {
-  Signal input{1.0, 1.0};
+  muslib::mixer::Signal input{1.0, 1.0};
 
-  EXPECT_DOUBLE_EQ(avg(input), 1.0);
+  EXPECT_DOUBLE_EQ(muslib::mixer::avg(input), 1.0);
 }
 
 TEST(Frame, OneValue) {
-  Signal2d expected{
+  muslib::mixer::Signal2d expected{
       {1.0},
   };
-  Signal input{1.0};
-  Signal2d received = frame(input, 1, 1);
+  muslib::mixer::Signal input{1.0};
+  muslib::mixer::Signal2d received = muslib::mixer::frame(input, 1, 1);
   compare_signal2d(expected, received);
 }
-} // namespace muslib::mixer
