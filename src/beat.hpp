ac71f8c804c7cd9dcc6ccedb7ca14dc0f1816049
Konrad Pióro <01150145@pw.edu.pl>
2025-01-12 03:27:09 +0100

    beat.hpp docs v1

--- src/beat.hpp
+++ src/beat.hpp
@@ -3,7 +3,20 @@
 
 namespace muslib::beat {
 
+/**
+ * @brief estimates tempo of given Signal.
+ * 
+ * @param signal A reference to the signal.
+ * @return Estimated tempo value as a double.
+ */
 double estimate_tempo(const Signal1 &signal);
+
+/**
+ * @brief detects beats of given Signal.
+ * 
+ * @param signal A reference to the signal.
+ * @return Beats value as a Time Vector.
+ */
 std::vector<Time> detect_beats(const Signal1 &signal);
 
 } // namespace muslib::beat
